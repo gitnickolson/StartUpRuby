66d76c794eed01261f6219d06777b6468806fc87
Niclas Eitelmann <niclas.eitelmann@MacNickolson.world.domination>
2023-08-08 09:18:16 +0200

    Initial commit

---
+++ substrings.rb
@@ -0,0 +1,27 @@
+dictionary = ["below","down","go","going","horn","how",
+              "howdy","it","i","low","own","part","partner","sit"]
+
+def substrings(words, dictionary)
+  words = words.downcase.gsub(",", "")
+  words_hash = Hash.new()
+
+  dictionary.each do |d_word|
+    if words.include?(d_word)
+      words_hash[d_word] = words.scan(d_word).length
+    end
+  end
+
+  words.split(" ",).each do |split_word|
+    if !words_hash.has_key?(split_word)
+      words_hash[split_word] = 0
+    end
+  end
+
+  puts words_hash
+end
+
+words = gets.chomp
+substrings(words, dictionary)
+
+
+
